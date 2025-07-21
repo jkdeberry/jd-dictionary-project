134c2c8f5d1a3ec4d531f894189a57e021c273e2
jkdeberry <debejmac@gmail.com>
2025-07-21 08:05:00 -0400

    Build incomplete, deleted a line not needed

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,7 +15,6 @@ export default function Dictionary() {
     // documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "4e2df5aotaa983694533f2b4440ef095";
     let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    let word = keyword
     axios.get(apiUrl).then(handleResponse);
 }
 
