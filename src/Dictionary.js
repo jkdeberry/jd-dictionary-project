e232963431e6efc49add2c06f72030dfee555aef
jkdeberry <debejmac@gmail.com>
2025-07-21 08:12:33 -0400

    Added documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,7 +12,8 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault()
 
-    // documentation: https://www.shecodes.io/learn/apis/dictionary
+    // documentation: https://www.shecodes.io/learn/apis/dictionary //
+
     let apiKey = "4e2df5aotaa983694533f2b4440ef095";
     let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
