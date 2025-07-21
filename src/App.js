7c6413bdb44ab18133eecf59526e7f11b51ad88e
jkdeberry <debejmac@gmail.com>
2025-07-21 06:59:30 -0400

    Added the search engine function

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export default function App() {
         <main>
           <Dictionary />
         </main>
-        <footer className="text-center">
-        Coded by SheCodes
+        <footer className="App-footer">
+        <small>Coded by SheCodes</small>
         </footer>
       </div>
     </div>
